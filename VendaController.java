6f282503601d9d999372e006e29c59bce79b83dc
Ângelo Vinicius Hernandez Mazarin <106436644+AngeloMzn@users.noreply.github.com>
2023-10-30 15:19:18 -0400

    Add files via upload

---
+++ VendaController.java
@@ -0,0 +1,38 @@
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+
+public class VendaController {
+
+    @FXML
+    private TextField descricao_itemVenda;
+
+    @FXML
+    private TextField id_ItemVenda;
+
+    @FXML
+    private TextField marca_ItemVenda;
+
+    @FXML
+    private TextField preco_ItemVenda;
+
+    @FXML
+    private TableView<?> tabela_venda;
+
+    @FXML
+    void btnBuscarItemVenda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void finalizarVenda(ActionEvent event) {
+
+    }
+
+    @FXML
+    void removerItemVenda(ActionEvent event) {
+
+    }
+
+}
